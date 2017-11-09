24d7c560721e1669a11b5899c62e6f52a76bdf3e
mgh3326 <mgh3326@naver.com>
2017-11-09 14:39:56 +0900

    complete4

--- kakao_algorithm_lv1_gcdlcm/gcdlcm.cpp
+++ kakao_algorithm_lv1_gcdlcm/gcdlcm.cpp
@@ -1,3 +1,9 @@
+/*
+최대공약수와 최소공배수 Level 1
+두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환해주는 gcdlcm 함수를 완성해 보세요.
+배열의 맨 앞에 최대공약수, 그 다음 최소공배수를 넣어 반환하면 됩니다.
+예를 들어 gcdlcm(3,12) 가 입력되면, [3, 12]를 반환해주면 됩니다.
+*/
 #include<vector>
 #include<iostream>
 using namespace std;
@@ -33,5 +39,6 @@ int main()
 	vector<int> testAnswer = gcdlcm(a, b);//유클리드 호제법 확인
 
 	cout << testAnswer[0] << " " << testAnswer[1];
+	//최대공약수 최소공배수 출력
 	return 0;
 }
