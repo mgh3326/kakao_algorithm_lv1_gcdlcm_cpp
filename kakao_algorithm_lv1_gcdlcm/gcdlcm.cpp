dabb3fb30ecd7fad5ebd1c82c48929bb0b28f3a5
mgh3326 <mgh3326@naver.com>
2017-11-09 14:21:43 +0900

    init

---
+++ kakao_algorithm_lv1_gcdlcm/gcdlcm.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+vector<int> gcdlcm(int a, int b)//test
+{
+	vector<int> answer;
+
+	return answer;
+}
+
+int main()
+{
+	int a = 3, b = 12;
+	vector<int> testAnswer = gcdlcm(a, b);
+
+	cout << testAnswer[0] << " " << testAnswer[1];
+}
